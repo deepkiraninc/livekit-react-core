83ce523d0c900b0930e2e4971994e8eb7d2081d1
Deep Kiran Foods <47971947+deepkiraninc@users.noreply.github.com>
2023-05-26 00:59:12 +0530

    Delete dist directory

--- dist/components/mediaDeviceSelect.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Room } from 'livekit-client';
-export declare function setupDeviceSelector(kind: MediaDeviceKind, room?: Room): {
-    className: string;
-    activeDeviceObservable: import("rxjs").Observable<string | undefined>;
-    setActiveMediaDevice: (id: string) => Promise<void>;
-};
-//# sourceMappingURL=mediaDeviceSelect.d.ts.map
