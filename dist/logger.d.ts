46abf7ecda8824049828876126597dd94b7d4bb1
root <root@ip-172-31-23-63.us-west-2.compute.internal>
2024-04-19 13:04:44 +0530

    Yarn built update from jenkins for core

--- dist/logger.d.ts
+++ dist/logger.d.ts
@@ -11,5 +11,15 @@ type SetLogLevelOptions = {
  * @public
  */
 export declare function setLogLevel(level: LogLevel, options?: SetLogLevelOptions): void;
+type LogExtension = (level: LogLevel, msg: string, context?: object) => void;
+type SetLogExtensionOptions = {
+    liveKitClientLogExtension?: LogExtension;
+};
+/**
+ * Set the log extension for both the `@livekit/components-react` package and the `@livekit-client` package.
+ * To set the `@livekit-client` log extension, use the `liveKitClientLogExtension` prop on the `options` object.
+ * @public
+ */
+export declare function setLogExtension(extension: LogExtension, options?: SetLogExtensionOptions): void;
 export {};
 //# sourceMappingURL=logger.d.ts.map
--- dist/observables/dataChannel.d.ts
+++ dist/observables/dataChannel.d.ts
@@ -1,15 +1,11 @@
-import type { LocalParticipant, Participant, Room } from 'livekit-client';
-import { DataPacket_Kind } from 'livekit-client';
+import type { DataPublishOptions, LocalParticipant, Participant, Room } from 'livekit-client';
 import { Observable } from 'rxjs';
 export declare const DataTopic: {
     readonly CHAT: "lk-chat-topic";
-};
-export type DataSendOptions = {
-    kind?: DataPacket_Kind;
-    destination?: string[];
+    readonly CHAT_UPDATE: "lk-chat-update-topic";
 };
 /** Publish data from the LocalParticipant. */
-export declare function sendMessage(localParticipant: LocalParticipant, payload: Uint8Array, topic?: string, options?: DataSendOptions): Promise<void>;
+export declare function sendMessage(localParticipant: LocalParticipant, payload: Uint8Array, options?: DataPublishOptions): Promise<void>;
 export interface BaseDataMessage<T extends string | undefined> {
     topic?: T;
     payload: Uint8Array;
@@ -17,13 +13,13 @@ export interface BaseDataMessage<T extends string | undefined> {
 export interface ReceivedDataMessage<T extends string | undefined = string> extends BaseDataMessage<T> {
     from?: Participant;
 }
-export declare function setupDataMessageHandler<T extends string>(room: Room, topic?: T, onMessage?: (msg: ReceivedDataMessage<T>) => void): {
+export declare function setupDataMessageHandler<T extends string>(room: Room, topic?: T | [T, ...T[]], onMessage?: (msg: ReceivedDataMessage<T>) => void): {
     messageObservable: Observable<{
         payload: Uint8Array;
         topic: T;
         from: import("livekit-client").RemoteParticipant | undefined;
     }>;
     isSendingObservable: Observable<boolean>;
-    send: (payload: Uint8Array, options?: DataSendOptions) => Promise<void>;
+    send: (payload: Uint8Array, options?: DataPublishOptions) => Promise<void>;
 };
 //# sourceMappingURL=dataChannel.d.ts.map
